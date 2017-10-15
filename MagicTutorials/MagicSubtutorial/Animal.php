545acaadc20cd8085bafb8d51cbd9fe5811cfb11
VytauteVita <vita.vytaute@gmail.com>
2017-10-15 20:50:23 +0300

    Other functions added

--- MagicTutorials/MagicSubtutorial/Animal.php
+++ MagicTutorials/MagicSubtutorial/Animal.php
@@ -11,8 +11,30 @@ class Animal {
     public $alive = true;
     public $color = '';
     public $size = '';
+    public $name = 'Do';
+    public $nickname = 'Plum';
 
-    
+
+    public function __call($name, $nickname)
+    {
+        print "The animal is called '$name' "
+            . implode('or ', $nickname). "\n";
+    }
+
+    public static function __callStatic($name, $nickname)
+    {
+        print "The animal is called '$name' "
+            . implode('or ', $nickname). "\n";
+    }
+
+    public function __clone() {
+        $this->name .= '++';
+    }
+
+    public function __toString($alive)
+    {
+        return $alive->alive;
+    }
 }
 
 
